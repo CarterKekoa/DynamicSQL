7c448a7ead5777a9b5f871d35c4262fdf49596ea
CarterMooring <carterkekoa@gmail.com>
2020-11-24 16:27:18 -0800

    pdf

--- hw8.java
+++ hw8.java
@@ -3,8 +3,9 @@
  * HW8
  * CPSC321 Databases
  * Nov. 21st, 2020
- * Description: 
- *
+ * Description: This program connects to a database previously made on ada.
+ * 				It acts as a simple test based interface for the DB that recieves input
+ * 				from a user and can perform actions on the DB such as deletes.
  */
 
 //To run: javac hw8.java
@@ -23,99 +24,99 @@ import java.util.Scanner;
 public class hw8 {
     public static void main(String[] args) {
 	try {
-	    // connection info
+	    // Info for Connection
 	    Properties prop = new Properties();
 	    FileInputStream in = new FileInputStream("config.properties");
 	    prop.load(in);
-	    in.close();
-	    // connect to datbase
+		in.close();
+		
+	    // Database Connection
 	    String hst = prop.getProperty("host");
 	    String usr = prop.getProperty("user");
 	    String pwd = prop.getProperty("password");
 	    String dab = "cmooring_DB";
 	    String url = "jdbc:mysql://" + hst + "/" + dab;
 	    Connection con = DriverManager.getConnection(url, usr, pwd);
-	    // START REAL PROGRAM HERE
-	    // create and execute query
+	    
+	    // Query are Created & Executed
 	    String q;
 	    ResultSet rs;
 	    int userInput;
 	    Scanner reader = new Scanner(System.in);
 	    do {
-		System.out.println("1. List countries\n2. Add country\n3. Find countries based on gdp and inflation\n4. Update country's gdp and inflation\n5. Exit");
-		System.out.print("Enter your choice (1-5):");
-		userInput = Integer.parseInt(reader.next());
-		if(userInput == 1) {
-		    q = "SELECT country_name, country_code FROM country";
-		    PreparedStatement pstmt = con.prepareStatement(q);
-		    rs = pstmt.executeQuery(q);
-		    // print results
-		    while(rs.next()) {
-			String catid = rs.getString("country_name");
-                        String name = rs.getString("country_code");
-                        System.out.println(catid + ", " + name);
-                    }
-		    pstmt.close();
-		    rs.close();
-		} else if (userInput == 2) {
-		    System.out.print("Country code................:");
-		    String countryCode = reader.next();
-		    System.out.print("Country name................:");
-		    String countryName = reader.next();
-		    System.out.print("Country per capida gdp (USD):");
-		    double gdp = Double.parseDouble(reader.next());
-		    System.out.print("Country inflation (pct).....:");
-		    double inflation = Double.parseDouble(reader.next());
-		    q = "INSERT INTO country VALUES(?,?,?,?)";
-		    PreparedStatement pstmt = con.prepareStatement(q);
-		    pstmt = con.prepareStatement(q);
-		    pstmt.setString(1,countryCode);
-		    pstmt.setString(2,countryName);
-		    pstmt.setDouble(3,gdp);
-		    pstmt.setDouble(4,inflation);
-		    pstmt.execute();
-		    pstmt.close();
-		} else if (userInput == 3) {
-		    System.out.print("Number of countries to display: ");
-		    int numCountries = Integer.parseInt(reader.next());
-		    System.out.print("Minimum per capita gdp (USD)..: ");
-		    double gdp = Double.parseDouble(reader.next());
-		    System.out.print("Maximum inflation (pct).......: ");
-		    double inflation = Double.parseDouble(reader.next());
-		    q = "SELECT * FROM country WHERE gdp>=? AND inflation<=? ORDER BY gdp DESC, inflation LIMIT ?";
-		    PreparedStatement pstmt = con.prepareStatement(q);
-		    pstmt.setDouble(1,gdp);
-		    pstmt.setDouble(2,inflation);
-		    pstmt.setInt(3,numCountries);
-		    rs = pstmt.executeQuery();
-		    while(rs.next()) {
-			String name = rs.getString("country_name");
-                        String cc = rs.getString("country_code");   
-                        double g = rs.getDouble("gdp");
-                        double i = rs.getDouble("inflation");
-                        System.out.println(name + " (" + cc + "), " + g + ", " + i);
-                    }
-		    rs.close();
-		    pstmt.close();
-		} else if(userInput == 4) {
-		    System.out.print("Country code: ");
-		    String cc = reader.next();
-		    System.out.print("Country per capita gdp (USD)..: ");
-		    double g = Double.parseDouble(reader.next());
-		    System.out.print("Country inflation (pct).......: ");
-		    double i = Double.parseDouble(reader.next());
-		    q = "UPDATE country SET gdp=?, inflation=? WHERE country_code=?";
-		    PreparedStatement pstmt = con.prepareStatement(q);
-		    pstmt.setDouble(1,g);
-		    pstmt.setDouble(2,i);
-		    pstmt.setString(3,cc);
-		    pstmt.execute();
-		}
+			System.out.println("1. List countries\n2. Add country\n3. Find countries based on gdp and inflation\n4. Update country's gdp and inflation\n5. Exit");
+			System.out.print("Enter your choice (1-5):");
+			userInput = Integer.parseInt(reader.next());
+			if(userInput == 1) {
+				q = "SELECT country_name, country_code FROM country";
+				PreparedStatement pstmt = con.prepareStatement(q);
+				rs = pstmt.executeQuery(q);
+				// print results
+				while(rs.next()) {
+				String catid = rs.getString("country_name");
+							String name = rs.getString("country_code");
+							System.out.println(catid + ", " + name);
+						}
+				pstmt.close();
+				rs.close();
+			} else if (userInput == 2) {
+				System.out.print("Country code................:");
+				String countryCode = reader.next();
+				System.out.print("Country name................:");
+				String countryName = reader.next();
+				System.out.print("Country per capida gdp (USD):");
+				double gdp = Double.parseDouble(reader.next());
+				System.out.print("Country inflation (pct).....:");
+				double inflation = Double.parseDouble(reader.next());
+				q = "INSERT INTO country VALUES(?,?,?,?)";
+				PreparedStatement pstmt = con.prepareStatement(q);
+				pstmt = con.prepareStatement(q);
+				pstmt.setString(1,countryCode);
+				pstmt.setString(2,countryName);
+				pstmt.setDouble(3,gdp);
+				pstmt.setDouble(4,inflation);
+				pstmt.execute();
+				pstmt.close();
+			} else if (userInput == 3) {
+				System.out.print("Number of countries to display: ");
+				int numCountries = Integer.parseInt(reader.next());
+				System.out.print("Minimum per capita gdp (USD)..: ");
+				double gdp = Double.parseDouble(reader.next());
+				System.out.print("Maximum inflation (pct).......: ");
+				double inflation = Double.parseDouble(reader.next());
+				q = "SELECT * FROM country WHERE gdp>=? AND inflation<=? ORDER BY gdp DESC, inflation LIMIT ?";
+				PreparedStatement pstmt = con.prepareStatement(q);
+				pstmt.setDouble(1,gdp);
+				pstmt.setDouble(2,inflation);
+				pstmt.setInt(3,numCountries);
+				rs = pstmt.executeQuery();
+				while(rs.next()) {
+				String name = rs.getString("country_name");
+							String cc = rs.getString("country_code");   
+							double g = rs.getDouble("gdp");
+							double i = rs.getDouble("inflation");
+							System.out.println(name + " (" + cc + "), " + g + ", " + i);
+						}
+				rs.close();
+				pstmt.close();
+			} else if(userInput == 4) {
+				System.out.print("Country code: ");
+				String cc = reader.next();
+				System.out.print("Country per capita gdp (USD)..: ");
+				double g = Double.parseDouble(reader.next());
+				System.out.print("Country inflation (pct).......: ");
+				double i = Double.parseDouble(reader.next());
+				q = "UPDATE country SET gdp=?, inflation=? WHERE country_code=?";
+				PreparedStatement pstmt = con.prepareStatement(q);
+				pstmt.setDouble(1,g);
+				pstmt.setDouble(2,i);
+				pstmt.setString(3,cc);
+				pstmt.execute();
+			}
 	    } while(userInput != 5);
-	    // release resources
+	    // Resources released
 	    con.close();
 	} catch(Exception err) {
-	    // do something useful
 	    err.printStackTrace();
 	}
     }
--- hw8input.php
+++ hw8input.php
@@ -1,43 +1,51 @@
 <html>
 <body>
-	<h1> HW 8 input </h1>
+	<h1> HW 8 Input </h1>
 	<form action='hw8output.php' method='post'>
-	<select name="country_code">
-		<?php
-			$config = parse_ini_file("../private/config.ini");
-			$server = $config["servername"];
-			$username = $config["username"];
-			$password = $config["password"];
-			$database = "cmooring_DB";
+		<select name="country_code">
+			<?php
+				/*
+				* Carter Mooring
+				* hw8output.php
+				* CPSC321 Databases
+				* Nov. 24th, 2020
+				* Description: This program connects to a database previously made on ada.
+				* 				It will provide the user with DB information on their selection from 
+				*				hw8input.php. 
+				* Info:	This is displayed on https://barney.gonzaga.edu/~cmooring/hw8output.php
+				*/
+				$config = parse_ini_file("../private/config.ini");
+				$server = $config["servername"];
+				$username = $config["username"];
+				$password = $config["password"];
+				$database = "cmooring_DB";
 
-			// connect
-			$conn = mysqli_connect($server, $username, $password, $database);
+				$conn = mysqli_connect($server, $username, $password, $database); // Database Coneection
 
-			// check connection
-			if (!$conn) {
-			   die("Connection failed: " . mysqli_connect_error()); 
-			}
-				
-			$query = "SELECT country_code, country_name FROM Country";
-				
-			$stmt = $conn->stmt_init();
-			$stmt->prepare($query);
+				// Runs if connection fails
+				if (!$conn) {
+				die("Connection failed: " . mysqli_connect_error()); 
+				}
+					
+				$query = "SELECT country_code, country_name FROM country";
+				$stmt = $conn->stmt_init();
+				$stmt->prepare($query);
 
-			// execute the statement and bind the result
-			$stmt->execute();
-			$stmt->bind_result($country_code, $country_name);
-			$stmt->fetch();
-			
-			while($row = $stmt->fetch()) {
-			   echo '<option value=' . $country_code . '>' . $country_name . '</option>';
-			}
+				// Execute query above
+				$stmt->execute();
+				$stmt->bind_result($country_code, $country_name);
+				$stmt->fetch();
 				
-			$stmt->close();
-			$conn->close();
-		?>
-	</select>
-	<input type='submit' value='submit'></input>
+				while($row = $stmt->fetch()) {
+				echo '<option value=' . $country_code . '>' . $country_name . '</option>';
+				}
+				
+				// finished so close
+				$stmt->close();
+				$conn->close();
+			?>
+		</select>
+		<input type='submit' value='submit'></input>
 	</form>
-	
 </body>
 </html>
